be5ec4cd709261900e87332725fc7beb38877fa1
Erik Milla <erikmilla456@gmail.com>
2025-06-10 16:12:28 -0500

    token

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 const productosRoutes = require('./routes/productos');
+const authRoutes = require('./routes/auth');
+const carritoRoutes = require('./routes/carrito');
 
 const app = express();
 const port = process.env.PORT || 3001;
@@ -10,6 +12,8 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/api', productosRoutes);
+app.use('/api/auth', authRoutes);
+app.use('/api/carrito', carritoRoutes);
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
